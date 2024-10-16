30c158b8ef25bc2a7950b8f60e0c99eb026410d7
Dillon Chapuli <148895731+dillonpachuli@users.noreply.github.com>
2024-10-16 16:36:02 +0000

    10/16/24

--- Main.java
+++ Main.java
@@ -1,4 +1,15 @@
-public static void main(String[] args)
+import java.util.ArrayList;
+
+public class Main
 {
-    WordChecker w = new WordChecker();
+    public static void main(String[] args)
+    {
+        WordChecker w = new WordChecker();
+
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("an");
+        words.add("band");
+        words.add("band");
+        words.add("abandon");
+    }
 }
