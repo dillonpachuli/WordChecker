46630206da4980fb01a4e1a8e077469f801ed575
Dillon Chapuli <148895731+dillonpachuli@users.noreply.github.com>
2024-11-07 17:12:19 +0000

    final

--- Main.java
+++ Main.java
@@ -9,20 +9,20 @@ public class Main
         words.add("band");
         words.add("band");
         words.add("abandon");
-        System.out.println(words);
+        System.out.println("words: " + words);
         WordChecker x = new WordChecker(words);
         System.out.println(x.isWordChain());
-        System.out.println("---------------------------------");
+        System.out.println("---------------------------------------------");
 
         ArrayList<String> words2 = new ArrayList<String>();
         words2.add("to");
         words2.add("too");
         words2.add("stool");
         words2.add("tools");
-        System.out.println(words2);
+        System.out.println("words2: " + words2);
         WordChecker y = new WordChecker(words2);
         System.out.println(y.isWordChain());
-        System.out.println("---------------------------------");
+        System.out.println("---------------------------------------------");
 
         ArrayList<String> words3 = new ArrayList<String>();
         words3.add("catch");
@@ -30,7 +30,10 @@ public class Main
         words3.add("catchacat");
         words3.add("cat");
         words3.add("at");
-        System.out.println(words3);
-        createList("cat");
+        System.out.println("words3: " + words3);
+        WordChecker z = new WordChecker(words3);
+        System.out.println(z.createList("cat"));
+        System.out.println(z.createList("catch"));
+        System.out.println(z.createList("dog"));
     }
 }
